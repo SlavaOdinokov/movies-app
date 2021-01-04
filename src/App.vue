d3ad2e497f7ca96dea3a10e6ca52a3378fe59a3b
Vyacheslav <odin.slavy@yandex.ru>
2021-01-04 19:03:08 +0300

    change background when hovering over poster

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="app">
-    <MoviesList :list="moviesList"/>
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <PosterBg :poster="posterBg" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import MoviesList from '@/components/MoviesList'
+import PosterBg from '@/components/PosterBg'
 
 export default {
   name: "App",
   components: {
-    MoviesList
+    MoviesList,
+    PosterBg
   },
+  data: () => ({
+    posterBg: ''
+  }),
   computed: {
     ...mapGetters('movies', ['moviesList'])
   },
   methods: {
-    ...mapActions('movies', ['fetchMovies'])
+    ...mapActions('movies', ['fetchMovies']),
+    onChangePoster(poster) {
+      this.posterBg = poster
+    }
   }
 };
 </script>
 
 <style>
 #app {
+  position: relative;
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/PosterBg.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="poster-bg" :style="posterStyle"></div>
+</template>
+
+<script>
+export default {
+  name: "PosterBg",
+  props: {
+    poster: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {},
+  data: () => ({
+    defaultPosterBg: "linear-gradient(45deg,rgb(0, 3, 38) 0%,rgb(82, 15, 117) 100%)"
+  }),
+  computed: {
+    posterStyle() {
+      return {
+        'background-image': this.posterBg
+      }
+    },
+    posterBg() {
+      return this.poster ? `url(${this.poster})` : this.defaultPosterBg
+    }
+  }
+}
+</script>
+
+<style scoped>
+.poster-bg {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -2;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: all .2s ease;
+}
+.poster-bg::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  opacity: .7;
+  background-image: linear-gradient(45deg,rgb(0, 3, 38) 0%,rgb(82, 15, 117) 100%);
+}
+</style>
